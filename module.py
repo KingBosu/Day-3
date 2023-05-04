5f99b0276ae562f7f836f65d538e3953ed24bf59
KingBosu <mckaywilliamiii@gmail.com>
2023-05-04 18:06:08 -0400

    homework

---
+++ module.py
@@ -0,0 +1,14 @@
+
+
+def find_area():
+    length = input()
+    width = input()
+    area = int(length)*int(width)
+    return area
+
+def find_circumference():
+    radius = int(input())
+    r = radius
+    pie2 = 2*3.14
+    c = pie2*r
+    return c
